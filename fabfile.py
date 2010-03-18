fd5b122c3fd6145bfa068dfce1379759acc3b12f
gregor <gregor@c740148a-5d57-4aaf-bbc6-d7a508ce025f>
2010-03-18 18:52:52 +0000

    changes.
    git-svn-id: svn+ssh://theobaldgranger.com/svnroot/skeleton@3 c740148a-5d57-4aaf-bbc6-d7a508ce025f

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#from __future__ import with_statement
+from __future__ import with_statement
 import os
 from fabric.api import abort, cd, local, env, run, settings, sudo
 
