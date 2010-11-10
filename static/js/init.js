e680e1729ef1694f0d6eda2ec9d4d584a89a8d46
gregor <gregor@c740148a-5d57-4aaf-bbc6-d7a508ce025f>
2010-11-10 17:28:55 +0000

    Adding log() javascript function.
    git-svn-id: svn+ssh://theobaldgranger.com/svnroot/skeleton@45 c740148a-5d57-4aaf-bbc6-d7a508ce025f

--- static/js/init.js
+++ static/js/init.js
@@ -3,3 +3,14 @@
 
     });
 })(jQuery);
+
+// usage: log('inside coolFunc',this,arguments);
+// http://paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
+window.log = function(){
+    // store logs to an array for reference
+    log.history = log.history || [];
+    log.history.push(arguments);
+    if(this.console){
+        console.log( Array.prototype.slice.call(arguments) );
+    }
+};
