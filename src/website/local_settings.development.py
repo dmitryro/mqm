25cf4f1c7b7a1457c6d260051444f5f7f7a0200f
Gregor Müllegger <gregor@muellegger.de>
2014-02-26 14:24:21 +0100

    Removing django-debug-toolbar-autoreload package. We will gulp for that in the future.

--- src/website/local_settings.development.py
+++ src/website/local_settings.development.py
@@ -30,23 +30,4 @@ MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES + (
 
 INSTALLED_APPS = INSTALLED_APPS + (
     'debug_toolbar',
-    'debug_toolbar_autoreload',
-)
-
-DEBUG_TOOLBAR_PANELS = (
-    # default panels
-    'debug_toolbar.panels.version.VersionDebugPanel',
-    'debug_toolbar.panels.timer.TimerDebugPanel',
-    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
-    'debug_toolbar.panels.headers.HeaderDebugPanel',
-    #'debug_toolbar.panels.profiling.ProfilingDebugPanel',
-    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-    'debug_toolbar.panels.sql.SQLDebugPanel',
-    'debug_toolbar.panels.template.TemplateDebugPanel',
-    'debug_toolbar.panels.cache.CacheDebugPanel',
-    'debug_toolbar.panels.signals.SignalDebugPanel',
-    'debug_toolbar.panels.logger.LoggingPanel',
-
-    # autoreload panel
-    'debug_toolbar_autoreload.AutoreloadPanel',
 )
