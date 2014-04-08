7a4d1f336f8bc860e49962e533371c06425f35d1
Gregor Müllegger <gregor@muellegger.de>
2014-04-08 17:49:04 +0200

    Using angular specific static directory.

---
+++ static/app/main.js
@@ -0,0 +1,55 @@
+'use strict';
+
+(function () {
+
+  var app = angular.module('website', [
+      'ngCookies',
+      'ngResource',
+      'ngSanitize',
+      'ngRoute',
+      'ui.bootstrap'
+    ]);
+
+  /*
+   * Configuration
+   */
+
+  app.config(function($httpProvider) {
+    $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+  });
+
+  /*
+   * Routing
+   */
+
+  app.config(function ($routeProvider, $locationProvider, urlconfig) {
+    $locationProvider.html5Mode(true).hashPrefix('!');
+    $routeProvider
+      .when('/', {
+        templateUrl: urlconfig.partials.prefix + 'index.html',
+        controller: 'Homepage'
+      })
+      .otherwise({
+        templateUrl: urlconfig.partials.prefix + '404.html',
+        controller: 'NotFound'
+      });
+  });
+
+  /*
+   * Main Controller
+   */
+
+  app.controller('App', function ($scope, $routeParams, $location, Project) {
+
+  });
+
+  app.controller('Homepage', function ($scope) {
+    // Homepage Ctrl
+  });
+
+  app.controller('NotFound', function ($scope) {
+    // Page not found.
+  });
+
+
+})();
