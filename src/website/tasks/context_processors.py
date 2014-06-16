ab46e94a10fbb69ea0ca6ba868289ac327c7192e
Gregor Müllegger <gregor@muellegger.de>
2014-06-16 21:06:53 +0200

    Fix tasks context processor if you are not logged in.

--- src/website/tasks/context_processors.py
+++ src/website/tasks/context_processors.py
@@ -2,9 +2,10 @@ from .models import Task
 
 
 def tasks(request):
-    tasks = Task.objects.all()
-    tasks = tasks.filter(assigned_to=request.user)
-    tasks = tasks.filter(done=False)
-    return {
-        'my_active_tasks': tasks,
-    }
+    if request.user.is_authenticated():
+        tasks = request.user.tasks.all()
+        tasks = tasks.filter(done=False)
+        return {
+            'my_active_tasks': tasks,
+        }
+    return {}
