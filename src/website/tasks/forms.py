40143e870c874fc8b5e7a394cca7bb9710dff413
Gregor Müllegger <gregor@muellegger.de>
2014-06-16 22:04:42 +0200

    Handling privileges correctly for tasks and assigned to field.

--- src/website/tasks/forms.py
+++ src/website/tasks/forms.py
@@ -16,9 +16,15 @@ class TaskForm(ModelForm):
     def __init__(self, *args, **kwargs):
         self.user = kwargs.pop('user')
         super(TaskForm, self).__init__(*args, **kwargs)
-        self.fields['assigned_to'].queryset = self.user.local_mind.users.all()
+        if self.user.privileges == self.user.SUPERUSER:
+            self.fields['assigned_to'].queryset = self.user.local_mind.users.all()
+            self.initial.setdefault('assigned_to', self.user)
+        else:
+            del self.fields['assigned_to']
 
     def save(self, *args, **kwargs):
         self.instance.user = self.user
         self.instance.local_mind = self.user.local_mind
+        if self.user.privileges != self.user.SUPERUSER:
+            self.instance.assigned_to = self.user
         return super(TaskForm, self).save(*args, **kwargs)
