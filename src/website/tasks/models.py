4491764461c728ee095a5e5543675dac9e356065
Gregor Müllegger <gregor@muellegger.de>
2014-06-16 17:08:52 +0200

    Rename task's created_by to user RUN: python manage.py migrate

--- src/website/tasks/models.py
+++ src/website/tasks/models.py
@@ -7,6 +7,8 @@ from django_extensions.db.fields import (AutoSlugField, CreationDateTimeField,
 
 class Task(models.Model):
     local_mind = models.ForeignKey('local_minds.LocalMind', related_name='tasks')
+    user = models.ForeignKey('accounts.User', verbose_name=_('Created by'),
+                             related_name='created_tasks')
 
     slug = AutoSlugField(unique=True, populate_from='text')
     text = models.CharField(_('Description'), max_length=141, help_text=_('limited to 141 characters'))
@@ -20,7 +22,6 @@ class Task(models.Model):
     assigned_to = models.ForeignKey('accounts.User', null=True, blank=True,
         db_index=True,
         related_name='tasks')
-    created_by = models.ForeignKey('accounts.User', related_name='created_tasks')
 
     created = CreationDateTimeField()
     modified = ModificationDateTimeField()
@@ -40,7 +41,7 @@ class Task(models.Model):
 class Comment(models.Model):
     task = models.ForeignKey('Task', related_name='comments')
 
-    author = models.ForeignKey('accounts.User', related_name='task_comments')
+    user = models.ForeignKey('accounts.User', related_name='task_comments')
     text = models.TextField()
 
     created = CreationDateTimeField()
