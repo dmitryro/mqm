45e7d21e583594835b64a4771e89d91b4bbd164d
Angelo <angelo@ma-work.co.uk>
2014-06-27 16:50:11 +0100

    adding fundingmap to dashboard

--- static/js/dev/app.js
+++ static/js/dev/app.js
@@ -89,6 +89,7 @@ $(document).ready(function() {
 	{"col":4,"row":2,"size_x":1,"size_y":1, "name":"theLabNews"},
 
   {"col":1,"row":3,"size_x":1,"size_y":1, "name":"videoStream"},
+  {"col":2,"row":3,"size_x":1,"size_y":1, "name":"fundingmap"},
 	/*
 	{"col":3,"row":2,"size_x":1,"size_y":1, "name":"buddySearch"},
 	{"col":1,"row":3,"size_x":1,"size_y":1, "name":"externalNews"},
