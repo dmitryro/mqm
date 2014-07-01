b1743be7b35d4c642211437ba91933f193030fa9
Gregor Müllegger <gregor@muellegger.de>
2014-07-01 16:59:03 +0200

    Protect development urls.

--- src/website/urls.py
+++ src/website/urls.py
@@ -1,12 +1,92 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
 from django.conf.urls import include, patterns, url
-from django.views.generic import RedirectView
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
+from django.shortcuts import render
+from django.views.generic import RedirectView
 
 admin.autodiscover()
+
+
+def protected_render(request, *args, **kwargs):
+    valid_request = False
+    if request.user.is_authenticated():
+        valid_request = True
+    if not valid_request:
+        raise Http404
+    return render(request, *args, **kwargs)
+
+
+development_urlpatterns = patterns('',
+
+    # Development static design templates
+    url(r'^d/my-dashboard/$', protected_render, {'template_name': 'dev/dashboard.html'}),
+    url(r'^d/local-dashboard/$', protected_render, {'template_name': 'dev/local-dashboard.html'}),
+    url(r'^d/video-stream/$', protected_render, {'template_name': 'dev/video.html'}),
+    url(r'^d/meet-the-team/$', protected_render, {'template_name': 'dev/meet-the-team.html'}),
+    url(r'^d/meet-the-team-member/$', protected_render, {'template_name': 'dev/team.html'}),
+    url(r'^d/local-mind/$', protected_render, {'template_name': 'dev/lm.html'}),
+    url(r'^d/events-diary/$', protected_render, {'template_name': 'dev/event-diary.html'}),
+    url(r'^d/my-local-area/$', protected_render, {'template_name': 'dev/local-map.html'}),
+    url(r'^d/funding-map/$', protected_render, {'template_name': 'dev/funding-map.html'}),
+    url(r'^d/todos/$', protected_render, {'template_name': 'dev/task.html'}),
+    url(r'^d/call-out/$', protected_render, {'template_name': 'dev/questions-and-answers.html'}),
+    url(r'^d/engine-room/$', protected_render, {'template_name': 'dev/documents.html'}),
+    url(r'^d/external-news/$', protected_render, {'template_name': 'dev/external-news.html'}),
+    url(r'^d/positive-news/$', protected_render, {'template_name': 'dev/positive-news.html'}),
+    url(r'^d/brand/$', protected_render, {'template_name': 'dev/brand.html'}),
+    url(r'^d/the-net-works/$', protected_render, {'template_name': 'dev/the-net-works.html'}),
+    url(r'^d/openhub-updates/$', protected_render, {'template_name': 'dev/openhub-updates.html'}),
+    url(r'^d/national-mind-updates/$', protected_render, {'template_name': 'dev/national-mind-updates.html'}),
+    url(r'^d/buddy-search/$', protected_render, {'template_name': 'dev/buddy-search.html'}),
+    url(r'^d/hot-topic-news/$', protected_render, {'template_name': 'dev/hot-topic-news.html'}),
+    url(r'^d/engine-room-2/$', protected_render, {'template_name': 'dev/engine-room.html'}),
+
+
+    url(r'^sign-up/part1$', protected_render, {'template_name': 'dev/signup/part1.html'}),
+    url(r'^sign-up/part2$', protected_render, {'template_name': 'dev/signup/part2.html'}),
+    url(r'^sign-up/part3$', protected_render, {'template_name': 'dev/signup/part3.html'}),
+    url(r'^sign-up/part4$', protected_render, {'template_name': 'dev/signup/part4.html'}),
+    url(r'^sign-up/part5$', protected_render, {'template_name': 'dev/signup/part5.html'}),
+    url(r'^sign-up/part6$', protected_render, {'template_name': 'dev/signup/part6.html'}),
+
+    #modals
+    #url(r'^_addClinicalResearch/$', protected_render, {'template_name': 'dev/modals/add-clinical-research.html'}),
+    #url(r'^_addExternalNews/$', protected_render, {'template_name': 'dev/modals/add-external-news.html'}),
+    url(r'^_addPositiveNews/$', protected_render, {'template_name': 'dev/modals/add-positive-news.html'}),
+    url(r'^_addService/$', protected_render, {'template_name': 'dev/modals/add-service.html'}),
+    url(r'^_addEvent/$', protected_render, {'template_name': 'dev/modals/add-event.html'}),
+    url(r'^_addKeyPartner/$', protected_render, {'template_name': 'dev/modals/add-key-partner.html'}),
+    url(r'^_addFunding/$', protected_render, {'template_name': 'dev/modals/add-funding.html'}),
+    url(r'^_addTodo/$', protected_render, {'template_name': 'dev/modals/add-todo.html'}),
+    url(r'^_addQuestion/$', protected_render, {'template_name': 'dev/modals/add-question.html'}),
+    url(r'^_addDocument/$', protected_render, {'template_name': 'dev/modals/add-document.html'}),
+    url(r'^_addNews/$', protected_render, {'template_name': 'dev/modals/add-news.html'}),
+    url(r'^_addVideo/$', protected_render, {'template_name': 'dev/modals/add-video.html'}),
+    url(r'^_addPassword/$', protected_render, {'template_name': 'dev/modals/change-password.html'}),
+    url(r'^_addResource/$', protected_render, {'template_name': 'dev/modals/add-resource.html'}),
+    url(r'^_addAnswer/$', protected_render, {'template_name': 'dev/modals/add-answer.html'}),
+
+
+    #calendar widget urls
+    url(r'^calendarTemplates/day.html$', protected_render, {'template_name': 'calendarTemplates/day.html'}),
+    url(r'^calendarTemplates/events-list.html$', protected_render, {'template_name': 'calendarTemplates/events-list.html'}),
+    url(r'^calendarTemplates/modal.html$', protected_render, {'template_name': 'calendarTemplates/modal.html'}),
+    url(r'^calendarTemplates/month-day.html$', protected_render, {'template_name': 'calendarTemplates/month-day.html'}),
+    url(r'^calendarTemplates/month.html$', protected_render, {'template_name': 'calendarTemplates/month.html'}),
+    url(r'^calendarTemplates/week-days.html$', protected_render, {'template_name': 'calendarTemplates/week-days.html'}),
+    url(r'^calendarTemplates/week.html$', protected_render, {'template_name': 'calendarTemplates/week.html'}),
+    url(r'^calendarTemplates/year-month.html$', protected_render, {'template_name': 'calendarTemplates/year-month.html'}),
+    url(r'^calendarTemplates/year.html$', protected_render, {'template_name': 'calendarTemplates/year.html'}),
+    #
+
+    #general
+     url(r'^log-in/$', protected_render, {'template_name': 'dev/login.html'}),
+     url(r'^invitee/$', protected_render, {'template_name': 'dev/invitee.html'})
+)
 
 urlpatterns = patterns('',
     url(r'^robots.txt$', 'django.shortcuts.render', {'template': 'robots.txt'},),
@@ -85,70 +165,7 @@ urlpatterns = patterns('',
     #url(r'^document/\+(?P<category_slug>[^/]+)/$', 'website.documents.views.document_list', name='document'),
     #url(r'^document/(?P<slug>[^/]+)/$', 'website.documents.views.document_detail', name='document'),
 
-    # Development static design templates
-    url(r'^d/my-dashboard/$', 'django.shortcuts.render', {'template_name': 'dev/dashboard.html'}),
-    url(r'^d/local-dashboard/$', 'django.shortcuts.render', {'template_name': 'dev/local-dashboard.html'}),
-    url(r'^d/video-stream/$', 'django.shortcuts.render', {'template_name': 'dev/video.html'}),
-    url(r'^d/meet-the-team/$', 'django.shortcuts.render', {'template_name': 'dev/meet-the-team.html'}),
-    url(r'^d/meet-the-team-member/$', 'django.shortcuts.render', {'template_name': 'dev/team.html'}),
-    url(r'^d/local-mind/$', 'django.shortcuts.render', {'template_name': 'dev/lm.html'}),
-    url(r'^d/events-diary/$', 'django.shortcuts.render', {'template_name': 'dev/event-diary.html'}),
-    url(r'^d/my-local-area/$', 'django.shortcuts.render', {'template_name': 'dev/local-map.html'}),
-    url(r'^d/funding-map/$', 'django.shortcuts.render', {'template_name': 'dev/funding-map.html'}),
-    url(r'^d/todos/$', 'django.shortcuts.render', {'template_name': 'dev/task.html'}),
-    url(r'^d/call-out/$', 'django.shortcuts.render', {'template_name': 'dev/questions-and-answers.html'}),
-    url(r'^d/engine-room/$', 'django.shortcuts.render', {'template_name': 'dev/documents.html'}),
-    url(r'^d/external-news/$', 'django.shortcuts.render', {'template_name': 'dev/external-news.html'}),
-    url(r'^d/positive-news/$', 'django.shortcuts.render', {'template_name': 'dev/positive-news.html'}),
-    url(r'^d/brand/$', 'django.shortcuts.render', {'template_name': 'dev/brand.html'}),
-    url(r'^d/the-net-works/$', 'django.shortcuts.render', {'template_name': 'dev/the-net-works.html'}),
-    url(r'^d/openhub-updates/$', 'django.shortcuts.render', {'template_name': 'dev/openhub-updates.html'}),
-    url(r'^d/national-mind-updates/$', 'django.shortcuts.render', {'template_name': 'dev/national-mind-updates.html'}),
-    url(r'^d/buddy-search/$', 'django.shortcuts.render', {'template_name': 'dev/buddy-search.html'}),
-    url(r'^d/hot-topic-news/$', 'django.shortcuts.render', {'template_name': 'dev/hot-topic-news.html'}),
-    url(r'^d/engine-room-2/$', 'django.shortcuts.render', {'template_name': 'dev/engine-room.html'}),
-
-
-    url(r'^sign-up/part1$', 'django.shortcuts.render', {'template_name': 'dev/signup/part1.html'}),
-    url(r'^sign-up/part2$', 'django.shortcuts.render', {'template_name': 'dev/signup/part2.html'}),
-    url(r'^sign-up/part3$', 'django.shortcuts.render', {'template_name': 'dev/signup/part3.html'}),
-    url(r'^sign-up/part4$', 'django.shortcuts.render', {'template_name': 'dev/signup/part4.html'}),
-    url(r'^sign-up/part5$', 'django.shortcuts.render', {'template_name': 'dev/signup/part5.html'}),
-    url(r'^sign-up/part6$', 'django.shortcuts.render', {'template_name': 'dev/signup/part6.html'}),
-
-    #modals
-    #url(r'^_addClinicalResearch/$', 'django.shortcuts.render', {'template_name': 'dev/modals/add-clinical-research.html'}),
-    #url(r'^_addExternalNews/$', 'django.shortcuts.render', {'template_name': 'dev/modals/add-external-news.html'}),
-    url(r'^_addPositiveNews/$', 'django.shortcuts.render', {'template_name': 'dev/modals/add-positive-news.html'}),
-    url(r'^_addService/$', 'django.shortcuts.render', {'template_name': 'dev/modals/add-service.html'}),
-    url(r'^_addEvent/$', 'django.shortcuts.render', {'template_name': 'dev/modals/add-event.html'}),
-    url(r'^_addKeyPartner/$', 'django.shortcuts.render', {'template_name': 'dev/modals/add-key-partner.html'}),
-    url(r'^_addFunding/$', 'django.shortcuts.render', {'template_name': 'dev/modals/add-funding.html'}),
-    url(r'^_addTodo/$', 'django.shortcuts.render', {'template_name': 'dev/modals/add-todo.html'}),
-    url(r'^_addQuestion/$', 'django.shortcuts.render', {'template_name': 'dev/modals/add-question.html'}),
-    url(r'^_addDocument/$', 'django.shortcuts.render', {'template_name': 'dev/modals/add-document.html'}),
-    url(r'^_addNews/$', 'django.shortcuts.render', {'template_name': 'dev/modals/add-news.html'}),
-    url(r'^_addVideo/$', 'django.shortcuts.render', {'template_name': 'dev/modals/add-video.html'}),
-    url(r'^_addPassword/$', 'django.shortcuts.render', {'template_name': 'dev/modals/change-password.html'}),
-    url(r'^_addResource/$', 'django.shortcuts.render', {'template_name': 'dev/modals/add-resource.html'}),
-    url(r'^_addAnswer/$', 'django.shortcuts.render', {'template_name': 'dev/modals/add-answer.html'}),
-
-
-    #calendar widget urls
-    url(r'^calendarTemplates/day.html$', 'django.shortcuts.render', {'template_name': 'calendarTemplates/day.html'}),
-    url(r'^calendarTemplates/events-list.html$', 'django.shortcuts.render', {'template_name': 'calendarTemplates/events-list.html'}),
-    url(r'^calendarTemplates/modal.html$', 'django.shortcuts.render', {'template_name': 'calendarTemplates/modal.html'}),
-    url(r'^calendarTemplates/month-day.html$', 'django.shortcuts.render', {'template_name': 'calendarTemplates/month-day.html'}),
-    url(r'^calendarTemplates/month.html$', 'django.shortcuts.render', {'template_name': 'calendarTemplates/month.html'}),
-    url(r'^calendarTemplates/week-days.html$', 'django.shortcuts.render', {'template_name': 'calendarTemplates/week-days.html'}),
-    url(r'^calendarTemplates/week.html$', 'django.shortcuts.render', {'template_name': 'calendarTemplates/week.html'}),
-    url(r'^calendarTemplates/year-month.html$', 'django.shortcuts.render', {'template_name': 'calendarTemplates/year-month.html'}),
-    url(r'^calendarTemplates/year.html$', 'django.shortcuts.render', {'template_name': 'calendarTemplates/year.html'}),
-    #
-
-    #general
-     url(r'^log-in/$', 'django.shortcuts.render', {'template_name': 'dev/login.html'}),
-     url(r'^invitee/$', 'django.shortcuts.render', {'template_name': 'dev/invitee.html'})
+    url(r'^', include(development_urlpatterns)),
 )
 
 app_patterns = patterns('',
