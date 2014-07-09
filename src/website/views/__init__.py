9206aa21076ef735df5cf116af8496cc9d60f6d6
Gregor Müllegger <gregor@muellegger.de>
2014-07-09 16:20:09 +0200

    Redirect to dashboard from frontpage.

--- src/website/views/__init__.py
+++ src/website/views/__init__.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-from .generic import CommonViewMixin
-
 
 def index(request):
-    return render_to_response('index.html', {
-    }, context_instance=RequestContext(request))
+    if request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('dashboard'))
+    return HttpResponseRedirect(reverse('login'))
 
 
 @login_required(login_url='/login/')
